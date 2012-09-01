aa729dedeccac9d5bc32e8ea1b38e747c0d47794
Kyle Temkin <ktemkin@binghamton.edu>
2012-08-31 20:04:48 -0400

    NOBUG updated extra plugin list

--- install_extra_plugins.sh
+++ install_extra_plugins.sh
@@ -17,10 +17,6 @@ echo '/mod/questionnaire' >> .git/info/exclude
 git clone git://github.com/mattgibson/moodle-block_ajax_marking.git -b $VERSION blocks/ajax_marking
 echo '/blocks/ajax_marking' >> .git/info/exclude
 
-# Configurable Reports Plugin
-git clone git://github.com/jleyva/moodle-block_configurablereports.git -b MOODLE_21_STABLE blocks/configurablereports
-echo '/blocks/configurablereports' >> .git/info/exclude
-
 # "Message My Teacher" block
 git clone git://github.com/marxjohnson/moodle-block_messageteacher.git blocks/messageteacher
 echo '/blocks/messageteacher' >> .git/info/exclude
@@ -29,6 +25,13 @@ echo '/blocks/messageteacher' >> .git/info/exclude
 git clone git://github.com/bumoodle/moodle-block_unanswered_discussions.git -b $VERSION blocks/unanswered_discussions
 echo '/blocks/unanswered_discussions' >> .git/info/exclude
 
+# Configurable Reports block
+git clone git://github.com/bumoodle/moodle-block_configurable_reports.git -b $VERSION blocks/configurable_reports
+echo '/blocks/configurable_reports' >> .git/info/exclude
+
 # "View as Example" block
 git clone git://github.com/moodleou/moodle-block_viewasexample.git blocks/viewasexample
 echo '/blocks/viewasexample' >> .git/info/exclude
+
+# Clean up exclude, if we're calling this multiple times.
+sort .git/info/exclude | uniq -u
--- update_extra_plugins.sh
+++ update_extra_plugins.sh
@@ -3,7 +3,7 @@
 #these are the locations (with respect to the Moodle install root) of each of the standard plugins to be updated
 extra_plugins=(
   'blocks/ajax_marking'
-  'blocks/configurablereports'
+  'blocks/configurable_reports'
   'blocks/messageteacher'
   'blocks/unanswered_discussions'
   'blocks/viewasexample'
@@ -12,5 +12,6 @@ extra_plugins=(
 
 #update each of the standard plugins
 for plugin in ${extra_plugins[@]}; do
+    echo "Updating $plugin" 
     git --git-dir=$plugin/.git pull 
 done
